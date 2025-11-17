9f3943f3570064510aab48258a6fdabcb565fe7c
jaehyeok-99 <wjklop4569@naver.com>
2025-11-17 16:19:42 +0900

    add vite project

---
+++ vite-project01/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <app-header v-bind=propsdata="str"></app-header>
+  </div>
+</template>
+
+<script setup>
+// 1. 우리가 만든 '헤더' 부품을 불러옵니다(import).
+import AppHeader from './components/AppHeader.vue'
+</script>
+
+<style scoped>
+main {
+  padding: 20px;
+  font-size: 1.2rem;
+  text-align: center;
+}
+</style>
---
+++ vite-project01/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+<header>
+    <h1>로봇 관제</h1>
+</header>  
+</template>
+
+<script>
+export default {
+    props: ['propsdata']
+}
+</script>
+
+<style>
+
+</style>
