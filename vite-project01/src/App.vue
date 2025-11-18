319bfe4221776ce5b1fe7d1d3b6ffa606a9ffcb2
jaehyeok-99 <wjklop4569@naver.com>
2025-11-18 16:41:55 +0900

    251118VueStudy-vue시작강의끝

--- vite-project01/src/App.vue
+++ vite-project01/src/App.vue
@@ -1,30 +1,47 @@
 <template>
+<form v-on:submit.prevent="submitform">
   <div>
-    <app-header
-      v-bind:propsdata='str' 
-      v-on:renew="renewStr"></app-header>
+    <label for="username">id: </label>
+    <input id="username" type="text" v-model="username">
   </div>
+  <div>
+    <label for="password">pw: </label>
+    <input id="password" type="password" v-model="password">
+  </div>
+  <button type="submit">Login</button>
+</form>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue'
+import axios from 'axios';
 
-export default{
-  data: function(){
-    return{
-      str: '로봇 관제'
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
     }
-  },
-  components:{
-    'app-header': AppHeader
   },
   methods: {
-    renewStr: function() {
-      this.str = 'Vue'
+    submitform: function() {
+      //event.preventDefault();
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      };
+      axios.post(url, data)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     }
   }
-}
+} 
 </script>
 
-<style scoped>
+<style>
 </style>
